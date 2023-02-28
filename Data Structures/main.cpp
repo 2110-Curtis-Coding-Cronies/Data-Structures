df45d6659f0cdc01a380c9dbdcd8e8e4b775fbd0
GraysonKippes <gkippes1@gmail.com>
2023-02-27 22:48:28 -0600

    Testing webhook integration.

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -5,17 +5,7 @@
 
 int main(void)
 {
-	DoublyLinkedList<int> dList1;
-
-	for (int i = 0; i < 10; i++) {
-		dList1.append((i + 1) * 5);
-	}
 	
-	size_t counter = 0;
-	for (DoubleNode<int>* node = dList1.getHead(); node != nullptr; node = node->next) {
-		std::cout << ++counter << ": " << node->value << '\n';
-	}
-	std::cout << std::endl;
 
 	return 0;
 }
